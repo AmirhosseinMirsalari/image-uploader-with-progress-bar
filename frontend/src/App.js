18f0df91e268a87958a80b34ce830dd650bc4921
Amirhossein Mirsalari <amir.tk7@gmail.com>
2022-12-12 21:08:23 +0330

    image uploader with progress bar

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import Content from './Content';
+import ShowData from './components/ShowData'
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+         <Route path="/" element={<Content />} />
+         <Route path="/gallery" element={<ShowData />} />
+    </Routes>
+ </BrowserRouter>
+  );
+}
+
+export default App;
